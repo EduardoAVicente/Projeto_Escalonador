dcd2ee27d238fb0e9044f86da2d9bd6ea753b740
Eduardo Antunes Vicente <97367813+EduardoAVicente@users.noreply.github.com>
2023-10-16 16:27:21 -0300

    atualizar

--- Escalonador/Main.java
+++ Escalonador/Main.java
@@ -1,5 +1,7 @@
 package Escalonador;
 
+
+
 public class Main {
     public static void main(String[] args) {
         Leitor leitor = new Leitor();
--- Escalonador/RoundRobin.java
+++ Escalonador/RoundRobin.java
@@ -27,31 +27,42 @@ public class RoundRobin {
         cpu = processos.get(0);
         processos.remove(0);
 
-        while (processos.size() != 0) {
+        while (cpu != null) {
             // roda o tempo
             jobutil.espera();
 
             // Sistema de logs
             System.out.println("********** TEMPO " + jobutil.getTime() + " **************");
-            //LOGS DE EVETOS
+            // LOGS DE EVETOS
             if (processos.get(0).getChegada() == jobutil.getTime()) {// adiciona novos pocessos
                 System.out.println("#[evento] CHEGADA <" + processos.get(0).getPID() + ">");
             }
 
-            //LOGS DE MONITORIAÇÃO
+            // LOGS DE MONITORIAÇÃO
             if (fila.size() == 0) {
                 System.out.println("FILA: Nao ha processos na fila");
             } else {
                 System.out.print("FILA: ");
                 for (int i = 0; i < fila.size(); i++) {
-                    System.out.print(fila.get(i).getPID() + " ");
+                    System.out.print(fila.get(i).getPID() + "(" + fila.get(i).getTempo() + ")" + " ");
                 }
                 System.out.println();
             }
 
             System.out.println("CPU: " + cpu.getPID() + "(" + cpu.getTempo() + ")");
 
+            // processos
+
             cpu.tempo();
+
+            if (cpu.getTempo() == 0) {
+                if (processos.size() == 0) {
+                    cpu = null;
+                } else {
+                    cpu = processos.get(0);
+                }
+
+            }
 
             if (processos.get(0).getChegada() == jobutil.getTime()) {// adiciona novos pocessos
                 fila.adicionar(cpu);
